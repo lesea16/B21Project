aad73fcae3468edf10dff75150bd244ba11b26d2
lesea16 <codru1993@gmail.com>
2020-11-10 11:51:21 -0600

    project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -4,5 +4,11 @@ public class Hello {
     // Mac user and windows users are happy to see commit dialog finally
     public static void main(String[] args) {
         System.out.println("Today is great day!");
+
+        // i have already published on Github.com
+        // now i am ready to make more commits
+        // but do i need to share again??
+        // Absolutely no
+        // it's time commit and just push!!!!
     }
 }
